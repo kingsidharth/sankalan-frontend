238f1f7cb080f609a6a9518957cf0a12466675fc
King Sidharth <king.sidharth@yahoo.com>
2017-04-06 15:44:14 +0530

    Utility classes for display

--- app/views/default.js
+++ app/views/default.js
@@ -27,7 +27,11 @@ class Header extends React.Component {
   render() {
     return(
       <div className="c-header o-layout__item u-1/1 u-background-black">
-        <div id="logo">
+        <div id="js-main-navigation" className="u-1/10 u-display-inline-block">
+          <a href="#"><i className="material-icons">menu</i></a>
+        </div>
+
+        <div id="logo" className="u-3/10 u-display-inline-block">
           <h1 className="u-logo">{ this.props.title }</h1>
         </div>
       </div>
